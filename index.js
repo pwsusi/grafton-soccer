b7c3736d5a14f2f768647ae276dc53d8c0e5ef04
Paul Susi <pwsusi@gmail.com>
2017-04-21 10:22:39 -0400

    echo

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ restService.get('/test', function (req, res) {
         });
 
 });
+
+restService.post('/echo', function(req, res) {
+    var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'webhook-echo-sample'
+    });
+});
+
 restService.post('/hook', function (req, res) {
 
     console.log('hook request');
