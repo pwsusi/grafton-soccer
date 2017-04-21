0d0f9ab175366f6b096b5f4a27e27a443e2b1b05
Paul Susi <pwsusi@gmail.com>
2017-04-21 13:13:10 -0400

    next game

--- index.js
+++ index.js
@@ -68,59 +68,22 @@ restService.post('/hook', function (req, res) {
     }
 });
 
-var games = [
-    {date: new Date( 2017, 6, 15, 16, 20 ), opponent: 'Shrewsbury', location: 'Lyon\'s field'},
-    {date: new Date( 2017, 3, 15, 16, 20 ), opponent: 'Worcester', location: 'Away field'},
+var events = [
+    {date: new Date( 2017, 6, 15, 16, 20 ), opponent: 'Shrewsbury', location: 'Lyon\'s field', type: 'game'},
+    {date: new Date( 2017, 3, 15, 16, 20 ), opponent: 'Worcester', location: 'Away field', type: 'game'},
+    {date: new Date( 2017, 3, 28, 15, 20 ), location: 'Lyon\'s field', type: 'practice'}
 ];
 
-restService.post('/game', function(req, res) {
+restService.post('/event', function(req, res) {
     //req.body.result && req.body.result.parameters && req.body.result.parameters.echoText
 
-    var options = {
-        weekday: "long", year: "numeric", month: "short",
-        day: "numeric", hour: "2-digit", minute: "2-digit"
-    };
-
-    var nextGame = getNextGame();
-    var speech = 'There was an unexpected error';
-    if(nextGame === undefined){
-        speech = 'You don\'t have any games left';
+    var speech = 'There was an unexpected error. I donn\'t know if you\'re asking about a practice or a game'';
+    if(req.body.result.parameters.requestType === 'game'){
+        speech = handleGameRequest(req);
+    }else if(req.body.result.parameters.requestType === 'practice'){
+        speech = handlePracticeRequest(req);
     }
-    else{
-       if(req.body.result.parameters.action === 'when'){
-            var outDate = nextGame.date.toLocaleTimeString("en-us", options);
-            speech = "Your next game is " + outDate;
-        }else if(req.body.result.parameters.action === 'who'){
-            if(nextGame.opponent === undefined){
-                speech = 'I\'m sorry, I\'m not sure who you\'re playing against';
-            }else{
-                speech = "Your next game is against " + nextGame.opponent;
-            }
-        }else if(req.body.result.parameters.action === 'where'){
-            if(nextGame.location === undefined){
-                speech = 'I\'m sorry, I\'m not sure where you\'re playing';
-            }else{
-                speech = "Your next game is scheduled to be played at " + nextGame.location;
-            }
-        }else{
-            var outDate = nextGame.date.toLocaleTimeString("en-us", options);
-            var opponent;
-            if(nextGame.opponent === undefined){
-                opponent = 'some team I\'m not sure about';
-            }else{
-                opponent = nextGame.opponent;
-            }
 
-            var location;
-            if(nextGame.location === undefined){
-                location = 'somewhere I\'m not sure about';
-            }else{
-                location = nextGame.location;
-            }
-
-            speech = "Your next game is " + outDate + " against "  + opponent + " and is scheduled to be played at " + location;
-        }
-    }
     return res.json({
             speech: speech,
             displayText: speech,
@@ -128,21 +91,109 @@ restService.post('/game', function(req, res) {
         });
 });
 
-function getNextGame(){
+function handleGameRequest(req){
+      var options = {
+            weekday: "long", year: "numeric", month: "short",
+            day: "numeric", hour: "2-digit", minute: "2-digit"
+        };
+
+        var nextGame = getNextEvent(req.body.result.parameters.requestType);
+        var speech = 'There was an unexpected error';
+        if(nextGame === undefined){
+            speech = 'You don\'t have any games left';
+        }
+        else{
+           if(req.body.result.parameters.action === 'when'){
+                var outDate = nextGame.date.toLocaleTimeString("en-us", options);
+                speech = "Your next game is " + outDate;
+            }else if(req.body.result.parameters.action === 'who'){
+                if(nextGame.opponent === undefined){
+                    speech = 'I\'m sorry, I\'m not sure who you\'re playing against';
+                }else{
+                    speech = "Your next game is against " + nextGame.opponent;
+                }
+            }else if(req.body.result.parameters.action === 'where'){
+                if(nextGame.location === undefined){
+                    speech = 'I\'m sorry, I\'m not sure where you\'re playing';
+                }else{
+                    speech = "Your next game is scheduled to be played at " + nextGame.location;
+                }
+            }else{
+                var outDate = nextGame.date.toLocaleTimeString("en-us", options);
+                var opponent;
+                if(nextGame.opponent === undefined){
+                    opponent = 'some team I\'m not sure about';
+                }else{
+                    opponent = nextGame.opponent;
+                }
+
+                var location;
+                if(nextGame.location === undefined){
+                    location = 'somewhere I\'m not sure about';
+                }else{
+                    location = nextGame.location;
+                }
+
+                speech = "Your next game is " + outDate + " against "  + opponent + " and is scheduled to be played at " + location;
+            }
+        }
+        return speech;
+};
+
+function handlePracticeRequest(req){
+      var options = {
+            weekday: "long", year: "numeric", month: "short",
+            day: "numeric", hour: "2-digit", minute: "2-digit"
+        };
+
+        var nextPractice = getNextEvent(req.body.result.parameters.requestType);
+        var speech = 'There was an unexpected error';
+        if(nextPractice === undefined){
+            speech = 'You don\'t have any practices left';
+        }
+        else{
+           if(req.body.result.parameters.action === 'when'){
+                var outDate = nextPractice.date.toLocaleTimeString("en-us", options);
+                speech = "Your next practice is " + outDate;
+            }else if(req.body.result.parameters.action === 'where'){
+                if(nextPractice.location === undefined){
+                    speech = 'I\'m sorry, I\'m not sure where you\'re practicing';
+                }else{
+                    speech = "Your next practice is at " + nextPractice.location;
+                }
+            }else{
+                var outDate = nextPractice.date.toLocaleTimeString("en-us", options);
+                var location;
+                if(nextPractice.location === undefined){
+                    location = 'somewhere I\'m not sure about';
+                }else{
+                    location = nextPractice.location;
+                }
+
+                speech = "Your next practice is " + outDate + "  at " + location;
+            }
+        }
+        return speech;
+};
+
+function getNextEvent(type){
     var startDate = new Date();
     var startTime = +startDate;
     var nearestDate, nearestDiff = Infinity;
-    var nextGame;
-    for( var i = 0, n = games.length;  i < n;  ++i ) {
-        var date = games[i].date;
+    var nextEvent;
+    for( var i = 0, n = events.length;  i < n;  ++i ) {
+        if(type !== events[i].type){
+            continue;
+        }
+        var date = events[i].date;
         var diff = +date - startTime;
         if( diff > 0  &&  diff < nearestDiff ) {
             nearestDiff = diff;
             nearestDate = date;
-            nextGame = games[i];
+            nextEvent = events[i];
         }
     }
-    return nextGame;
+    return nextEvent;
 };
 
 restService.listen((process.env.PORT || 5000), function () {
